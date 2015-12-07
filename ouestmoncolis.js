05c8e8a96f697ffe5bf82ec06be75e2d50825b2d
Benoit Richter <benoit.richter@opendatasoft.com>
2015-12-07 13:55:32 +0100

    Graphical revamp of demo page

--- ouestmoncolis.js
+++ ouestmoncolis.js
@@ -2,7 +2,7 @@
 var map = L.map('map').setView([48.856578,2.351828], 11);
 
 // Add a MapQuest layer
-L.tileLayer('http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+L.tileLayer('http://d.tiles.mapbox.com/v4/opendatasoft.gb69acb2/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoib3BlbmRhdGFzb2Z0IiwiYSI6Im9taEJndlkifQ.gN6NtnubxT8HJ-AUY2o_rg', {
             attribution: '&copy; <a href="www.openstreetmap.org/copyright">OpenStreetMap</a>'
           }).addTo(map);
 
@@ -16,7 +16,7 @@ function showData() {
         var jqxhr = $.ajax({url: "https://sales04-ssg.dev.ca.com:9443/suivi/" + idcolis})
             .success(function(result) {
 
-                var targetSite = L.marker([result["Fields"]["records"][0]["fields"]["longlat"][0], result["Fields"]["records"][0]["fields"]["longlat"][1]]).addTo(map);                
+                var targetSite = L.marker([result["Fields"]["records"][0]["fields"]["longlat"][0], result["Fields"]["records"][0]["fields"]["longlat"][1]]).addTo(map);
                 var currentPosition = L.marker([result["Statut"]["positionCamion"]["lat"], result["Statut"]["positionCamion"]["lng"]]).addTo(map);
 
                 targetSite.bindPopup("<b>Site de livraison</b>: " + result["Fields"]["records"][0]["fields"]["libelle_du_site"] + "<br /><b>Adresse</b>: " + result["Fields"]["records"][0]["fields"]["adresse"] + ", " +  result["Fields"]["records"][0]["fields"]["localite"] + "</center>");
