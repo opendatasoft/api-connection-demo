721f2179af6d6dae006a9346951e96ada96db9b8
Benoit Richter <benoit.richter@opendatasoft.com>
2015-12-07 14:35:39 +0100

    Map icons

--- ouestmoncolis.js
+++ ouestmoncolis.js
@@ -6,6 +6,9 @@ L.tileLayer('http://d.tiles.mapbox.com/v4/opendatasoft.gb69acb2/{z}/{x}/{y}.png?
             attribution: '&copy; <a href="www.openstreetmap.org/copyright">OpenStreetMap</a>'
           }).addTo(map);
 
+var truckIcon = L.divIcon({html: '<div class="map-icon truck-icon"><i class="fa fa-truck"></div>'});
+var postOfficeIcon = L.divIcon({html: '<div class="map-icon postoffice-icon"><i class="fa fa-envelope"></div>'});
+
 function showData() {
     // Get the identifier
     var idcolis = $("#idcolis").val();
@@ -16,8 +19,8 @@ function showData() {
         var jqxhr = $.ajax({url: "https://sales04-ssg.dev.ca.com:9443/suivi/" + idcolis})
             .success(function(result) {
 
-                var targetSite = L.marker([result["Fields"]["records"][0]["fields"]["longlat"][0], result["Fields"]["records"][0]["fields"]["longlat"][1]]).addTo(map);
-                var currentPosition = L.marker([result["Statut"]["positionCamion"]["lat"], result["Statut"]["positionCamion"]["lng"]]).addTo(map);
+                var targetSite = L.marker([result["Fields"]["records"][0]["fields"]["longlat"][0], result["Fields"]["records"][0]["fields"]["longlat"][1]], {icon: postOfficeIcon}).addTo(map);
+                var currentPosition = L.marker([result["Statut"]["positionCamion"]["lat"], result["Statut"]["positionCamion"]["lng"]], {icon: truckIcon}).addTo(map);
 
                 targetSite.bindPopup("<b>Site de livraison</b>: " + result["Fields"]["records"][0]["fields"]["libelle_du_site"] + "<br /><b>Adresse</b>: " + result["Fields"]["records"][0]["fields"]["adresse"] + ", " +  result["Fields"]["records"][0]["fields"]["localite"] + "</center>");
                 currentPosition.bindPopup("<b>Type de livraison</b>: " + result["Statut"]["base_label"] + "<br /><b>Dernière mise à jour</b>: " + result["Statut"]["date"])
